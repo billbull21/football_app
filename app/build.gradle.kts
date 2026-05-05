16110d31e7e287f1e1518a2d0fc64ba34dcdfcd7
khabilbul <oemampedia@gmail.com>
2026-05-06 03:33:08 +0700

    fix bug build github actions

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -8,12 +8,12 @@ plugins {
 
 android {
     namespace = "com.oemam.footballapp"
-    compileSdk = 35
+    compileSdk = 36
 
     defaultConfig {
         applicationId = "com.oemam.footballapp"
         minSdk = 24
-        targetSdk = 35
+        targetSdk = 36
         versionCode = 1
         versionName = "1.0"
 
--- core/build.gradle.kts
+++ core/build.gradle.kts
@@ -6,7 +6,7 @@ plugins {
 
 android {
     namespace = "com.oemam.footballapp.core"
-    compileSdk = 35
+    compileSdk = 36
 
     defaultConfig {
         minSdk = 24
